00e08c26d49f3a4aab5eaeeb88f44cddf60055ad
harshi5770 <110025165+harshi5770@users.noreply.github.com>
2022-07-27 16:02:12 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# Add implementation
+def add(x,y):
+	return x+y
+# subtraction Implementation
+def subtract(x,y):
+	return x-y		# on master branch
+# Multiply Implementation
+def multiply(x,y):
+	return x*y		# on bug456
+# Divide Implementation
+def divide(x,y):
+	if y == 0:		...
+		return DIVIDE_BY_0_ERROR
+	else:
+		return x/y
+def square(x):
+	return x*x
+
